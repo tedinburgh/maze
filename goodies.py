1c5f159e15559e88d40af91b7dfd8ec2e23782d6
Chris Wilson <chris.wilson@cantabcapital.com>
2018-11-26 10:35:14 +0000

    Add an example Goody for the players in the contest to rename and extend

--- goodies.py
+++ goodies.py
@@ -22,3 +22,10 @@ class RandomGoody(Goody):
         ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
         possibilities = [direction for direction in [UP, DOWN, LEFT, RIGHT] if not obstruction[direction]] + [PING]
         return random.choice(possibilities)
+
+class YourGoody(Goody):
+    ''' Your Goody implementation. Please change the name of this class to make it unique! '''
+
+    def take_turn(self, obstruction, _ping_response):
+        ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
+        # TODO. See RandomGoody.take_turn() above for examples.
